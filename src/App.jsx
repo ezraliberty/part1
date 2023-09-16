3cf112b864197d9a9441fa46e506728f89cc4c35
ezraliberty <ezraliberty99@gmail.com>
2023-09-16 18:53:58 +0100

    passed the props as an array

--- src/App.jsx
+++ src/App.jsx
@@ -13,9 +13,9 @@ const Part = (props) => {
 const Content = (props) => {
   return (
     <div>
-      <Part title={props.part1} exercises={props.exercises1} />
-      <Part title={props.part2} exercises={props.exercises2} />
-      <Part title={props.part3} exercises={props.exercises3} />
+      <Part title={props.parts[0].name} exercises={props.parts[0].exercises} />
+      <Part title={props.parts[1].name} exercises={props.parts[1].exercises} />
+      <Part title={props.parts[2].name} exercises={props.parts[2].exercises} />
     </div>
   );
 };
@@ -24,7 +24,7 @@ const Total = (props) => {
   return (
     <p>
       Number of exercises{" "}
-      {props.exercises1 + props.exercises2 + props.exercises3}
+      {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}
     </p>
   );
 };
@@ -49,19 +49,8 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content
-        part1={parts[0].name}
-        exercises1={parts[0].exercises}
-        part2={parts[1].name}
-        exercises2={parts[1].exercises}
-        part3={parts[2].name}
-        exercises3={parts[2].exercises}
-      />
-      <Total
-        exercises1={parts[0].exercises}
-        exercises2={parts[1].exercises}
-        exercises3={parts[2].exercises}
-      />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   );
 };
